0f942a072a82c8b460c8b745d6e269abe927ea55
SMNahuel <nahuelsan96@gmail.com>
2021-11-03 20:58:54 -0300

    _Init

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Home = () => {
+
+    return (
+        <div className="container">
+            <h1>Home</h1>
+
+        </div>
+
+    )
+}
+
+export default Home;
